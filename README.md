53a8acdc5055eabc5c5138076f7d6285884b9e7a
Erik Dasque <edasque@constantcontact.com>
2015-07-08 12:10:19 -0400

    Replaced aws2js & optparse with aws-sdk & config/minimist
    Thank you @stozlu for the optparse & minimist work!
    Had to squash a lot of private repo commits into this OSS repo.

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 AWS Cloudwatch2Graphite
 ==================
 
+TODO
+------------------
+1. Update Dockerfile to create a crontab so as to sendMetrics every 10 minutes
+2. ~~https://github.com/edasque/cloudwatch2graphite was written as a generic utility. When I recently moved it to using 'aws-sdk' instead of the unofficial 'aws2js', I hacked the genericity/configurability out. cw2graphite should again be configurable for multiple metrics~~
+    - This configurability should use the native node-config instead of optparse
+3. ~~Collect all 6 metrics from Cloudfront: Requests, BytesDownloaded,BytesUploaded, TotalErrorRate, 4xxErrorRate,5xxErrorRate~~
+4. Cloudwatch documentation says the Error metrics should be Average/Percent but setting the Statistics as Average leads to Undefined - let's figure it out
+4. Deploy to cloudfront2graphite.ctct.net qa Open Stack instance and make sure it works for a while
+5. Deploy to a production OpenStack instance (you need Centos 6.6 for Docker) that sends metics to the production Graphite instance
+6. Update the Cloudfront only dashboard
+7. Add Cloudfront metrics to the appropriate MLP dashboards
+
+What is this for?
+--------------------
+
 This application will output graphite counters for a list of AWS CloudWatch metrics. All you need to do is :
 
 * copy `conf/metrics.json.sample` into `conf/metrics.json`
--- cw2graphite.js
+++ cw2graphite.js
@@ -1,103 +1,67 @@
+var config = require('./lib/readConfig.js').readCmdOptions();
+
+// Now using the official Amazon Web Services SDK for Javascript
+var AWS = require("aws-sdk");
+
+// We'll use the Cloudwatch API
+var cloudwatch = new AWS.CloudWatch(config.awsCredentials);
+
+// pulling all of lodash for _.sortBy(), does it matter? Do we even need to sort?
+var _ = require('lodash');
+
+// TODO: do we need both those libraries, do we need any?
 var dateFormat = require('dateformat');
 require('./lib/date');
-var global_options = require('./lib/options.js').readCmdOptions();
 
-var cloudwatch = require('aws2js').load('cloudwatch', global_options.credentials.accessKeyId, global_options.credentials.secretAccessKey);
+// getting 10 minutes worth of data & 1 minute
+var interval = 11;
 
-cloudwatch.setRegion(global_options.region_name);
+// Between now and 11 minutes ago
+var now = new Date();
+var then = (interval).minutes().ago();
+var end_time = dateFormat(now, "isoUtcDateTime");
+var start_time = dateFormat(then, "isoUtcDateTime");
 
-var metrics = global_options.metrics_config.metrics
+// We used to use this when looking at Billing metrics
+// if ( metric.Namespace.match(/Billing/) ) {
+//     then.setHours(then.getHours() - 30)
+// }
+// if ( metric.Namespace.match(/Billing/) ) {
+//     options["Period"] = '28800'
+// }
 
-for(index in metrics) {
-	getOneStat(metrics[index],global_options.region_name);
+var metrics = config.metricsConfig.metrics;
+
+for (var index in metrics) {
+    printMetric(metrics[index], start_time, end_time);
 }
 
-function getOneStat(metric,regionName) {
-	var interval = 11;
+function printMetric(metric, get_start_time, get_end_time) {
 
-	var now = new Date();
-	var then = (interval).minutes().ago()
+    var getMetricStatistics_param = metric;
 
-	if ( metric.Namespace.match(/Billing/) ) {
-	    then.setHours(then.getHours() - 30)
-	}
+    getMetricStatistics_param.StartTime = get_start_time;
+    getMetricStatistics_param.EndTime = get_end_time;
 
-	var end_time = dateFormat(now, "isoUtcDateTime");
-	var start_time = dateFormat(then, "isoUtcDateTime");
+    cloudwatch.getMetricStatistics(getMetricStatistics_param, function (err, data) {
+        if (err) {
+            console.error(err, err.stack); // an error occurred
+            console.error("on:\n" + JSON.stringify(getMetricStatistics_param, null, 2));
+        }
+        else {
 
+            var dimension_prefix = "";
+            for (var dim_index in getMetricStatistics_param.Dimensions) {
+                dimension_prefix += "." + getMetricStatistics_param.Dimensions[dim_index].Name;
+                dimension_prefix += "_" + getMetricStatistics_param.Dimensions[dim_index].Value;
+            }
 
-	var options = {
-		Namespace: metric.Namespace,
-		MetricName: metric.MetricName,
-		Period: '60',
-		StartTime: start_time,
-		EndTime: end_time,
-		"Statistics.member.1": metric["Statistics.member.1"],
-		Unit: metric.Unit,
-	}
-
-	if ( metric.Namespace.match(/Billing/) ) {
-	    options["Period"] = '28800'
-	}
-
-	metric.name = (global_options.metrics_config.carbonNameSpacePrefix != undefined) ? global_options.metrics_config.carbonNameSpacePrefix + "." : "";
-	metric.name = metric.name.replace("{regionName}",regionName);
-	
-	metric.name += metric.Namespace.replace("/", ".");
-
-	for (var i=1;i<=10;i++) {
-		if (metric["Dimensions.member."+i+".Name"]!==undefined && metric["Dimensions.member."+i+".Value"]!==undefined) {
-			options["Dimensions.member."+i+".Name"] = metric["Dimensions.member."+i+".Name"]
-			options["Dimensions.member."+i+".Value"] = metric["Dimensions.member."+i+".Value"]
-
-			metric.name += "." + metric["Dimensions.member."+i+".Value"];
-		}
-	}
-	
-	metric.name += "." + metric.MetricName;
-	metric.name += "." + metric["Statistics.member.1"];
-	metric.name += "." + metric.Unit;
-
-	metric.name = metric.name.toLowerCase()
-
-	// console.log(metric);
-	cloudwatch.request('GetMetricStatistics', options, function(error, response) {
-		if(error) {
-			console.error("ERROR ! ",error);
-			return;
-		}
-		if (! response.GetMetricStatisticsResult) {
-			console.error("ERROR ! response.GetMetricStatisticsResult is undefined for metric " + metric.name);
-			return;
-		}
-		if (!response.GetMetricStatisticsResult.Datapoints) {
-			console.error("ERROR ! response.GetMetricStatisticsResult.Datapoints is undefined for metric " + metric.name);
-			return;
-		}
-			
-		var memberObject = response.GetMetricStatisticsResult.Datapoints.member;
-		if (memberObject == undefined) {
-			console.error("WARNING ! no data point available for metric " + metric.name);
-			return;
-		}
-
-		var dataPoints;
-		if(memberObject.length === undefined) {
-			dataPoints = [memberObject];
-		} else {
-			// samples might not be sorted in chronological order
-			dataPoints = memberObject.sort(function(m1,m2){
-				var d1 = new Date(m1.Timestamp), d2 = new Date(m2.Timestamp);
-				return d1 - d2
-			});
-		}
-		// Very often in Cloudwtch the last aggregated point is inaccurate and might be updated 1 or 2 minutes later
-		// this is not a problem if we choose to overwrite it into graphite, so we read the 3 last points.
-		if (dataPoints.length > global_options.metrics_config.numberOfOverlappingPoints) {
-			dataPoints = dataPoints.slice(dataPoints.length-global_options.metrics_config.numberOfOverlappingPoints, dataPoints.length);
-		}
-		for (var point in dataPoints) {
-			console.log("%s %s %s", metric.name, dataPoints[point][metric["Statistics.member.1"]], parseInt(new Date(dataPoints[point].Timestamp).getTime() / 1000.0));
-		}
-	});
+            sorted_data = _.sortBy(data.Datapoints, function (n) {
+                return n.Timestamp.getTime();
+            });
+            for (var point in sorted_data) {
+                console.log("%s %s %s", getMetricStatistics_param.Namespace.replace("/", ".") + dimension_prefix + "." + getMetricStatistics_param.MetricName, sorted_data[point].Sum, parseInt(new Date(sorted_data[point].Timestamp).getTime() / 1000.0));
+            }
+        }
+    });
 }
